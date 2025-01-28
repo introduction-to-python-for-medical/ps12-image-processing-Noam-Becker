edf2d323daa73c430178f8642036a26c2042562d
Noam-Becker <noambecker1999@gmail.com>
2025-01-28 13:52:33 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+import matplotlib.pyplot as plt
+from image_utils import load_image, edge_detection
+
+# טעינת התמונה המקורית
+image = load_image("path_to_your_image.jpg")
+
+# ביצוע זיהוי קצוות
+edges = edge_detection(image)
+
+# הצגת התמונה המקורית והתמונה עם הקצוות
+plt.subplot(1, 2, 1)
+plt.title("Original Image")
+plt.imshow(image, cmap='gray')
+
+plt.subplot(1, 2, 2)
+plt.title("Edge Detected Image")
+plt.imshow(edges, cmap='gray')
+
+plt.show()
